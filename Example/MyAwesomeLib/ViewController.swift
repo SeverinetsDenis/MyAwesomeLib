bcaee7bd10c330484c43118ad757eb59bac3e5c2
SeverinetsDenis <severinetsdenis@gmail.com>
2018-12-07 10:28:28 +0300

    Initial commit

---
+++ Example/MyAwesomeLib/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MyAwesomeLib
+//
+//  Created by SeverinetsDenis on 12/07/2018.
+//  Copyright (c) 2018 SeverinetsDenis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
